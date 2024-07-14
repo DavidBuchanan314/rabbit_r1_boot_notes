3cda53d2859a2fdac6c711efcd1c24164be9e965
David Buchanan <d@vidbuchanan.co.uk>
2024-07-14 12:21:09 +0100

    offset finder template generation

--- scripts/patchfind_lk_test.py
+++ scripts/patchfind_lk_test.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
+import os
 
 @dataclass
 class Pattern:
+	name: str
 	pattern: bytes
 	caremap: bytes
 	alignment: int  # stride to search with
 	offset: int     # offset relative to start of pattern data
 
-def find_pattern(data: bytes, pattern: Pattern):
+def find_pattern(data: bytes, pattern: Pattern, baseaddr: int):
 	results = []
 	for i in range(0, len(data) - len(pattern.pattern), pattern.alignment):
 		for j, (pat, care) in enumerate(zip(pattern.pattern, pattern.caremap)):
@@ -19,92 +21,117 @@ def find_pattern(data: bytes, pattern: Pattern):
 		raise Exception("Pattern not found")
 	if len(results) > 1:
 		raise Exception(f"Too many results ({results})")
-	return results[0] + pattern.offset
+	return baseaddr + results[0] + pattern.offset
 
-read_partition_pattern = Pattern(
-	pattern=bytes.fromhex(
-		"""
-		00000000
-		00000000
-		00000000
-		00000048
-		00000000
-		00000048
-		00000048
-		00000048
-		00000048
-		00000048
-		00000048
-		00000048
-		00000048
-		00000048
-		"""
+LK_BASE = 0x4800_0000
+
+PATTERNS = [
+	Pattern(
+		name="avb_hal_read_from_partition_ptr", # part of the avbops function pointer table
+		pattern=bytes.fromhex(
+			"""
+			00000000
+			00000000
+			00000000
+			00000048
+			00000000
+			00000048
+			00000048
+			00000048
+			00000048
+			00000048
+			00000048
+			00000048
+			00000048
+			00000048
+			"""
+		),
+		caremap = bytes.fromhex(
+			"""
+			ffffffff
+			ffffffff
+			ffffffff
+			000000ff
+			ffffffff
+			000000ff
+			000000ff
+			000000ff
+			000000ff
+			000000ff
+			000000ff
+			000000ff
+			000000ff
+			000000ff
+			"""
+		),
+		alignment=4,
+		offset=0xc
 	),
-	caremap = bytes.fromhex(
-		"""
-		ffffffff
-		ffffffff
-		ffffffff
-		000000ff
-		ffffffff
-		000000ff
-		000000ff
-		000000ff
-		000000ff
-		000000ff
-		000000ff
-		000000ff
-		000000ff
-		000000ff
-		"""
+	Pattern(
+		name="printf",
+		pattern=bytes.fromhex("0fb4 0000 f0b5 9bb0"),
+		caremap=bytes.fromhex("ffff 0000 ffff ffff"),
+		alignment=2, # thumb instructions
+		offset=0
 	),
-	alignment=4,
-	offset=0x4800_0000 + 0xc
-)
-
-printf_pattern = Pattern(
-	pattern=bytes.fromhex("0fb4 0000 f0b5 9bb0"),
-	caremap=bytes.fromhex("ffff 0000 ffff ffff"),
-	alignment=2, # thumb instructions
-	offset=0x4800_0000
-)
-
-handle_vboot_state_pattern = Pattern(
-	pattern=bytes.fromhex("30b5 83b0 02ab 0022"),
-	caremap=bytes.fromhex("ffff ffff ffff ffff"),
-	alignment=2, # thumb instructions
-	offset=0x4800_0000
-)
-
-lcd_printf_pattern = Pattern(
-	pattern=bytes.fromhex("0fb4 30b5 c9b0"),
-	caremap=bytes.fromhex("ffff ffff ffff"),
-	alignment=2, # thumb instructions
-	offset=0x4800_0000
-)
-
-memcpy_pattern = Pattern(
-	pattern=bytes.fromhex("000052e3 00005111 1eff2f01 31402de9"),
-	caremap=bytes.fromhex("ffffffff ffffffff ffffffff ffffffff"),
-	alignment=4, # non-thumb
-	offset=0x4800_0000
-)
+	Pattern(
+		name="handle_vboot_state",
+		pattern=bytes.fromhex("30b5 83b0 02ab 0022"),
+		caremap=bytes.fromhex("ffff ffff ffff ffff"),
+		alignment=2, # thumb instructions
+		offset=0
+	),
+	Pattern(
+		name="lcd_printf",
+		pattern=bytes.fromhex("0fb4 30b5 c9b0"),
+		caremap=bytes.fromhex("ffff ffff ffff"),
+		alignment=2, # thumb instructions
+		offset=0
+	),
+	Pattern(
+		name="memcpy",
+		pattern=bytes.fromhex("000052e3 00005111 1eff2f01 31402de9"),
+		caremap=bytes.fromhex("ffffffff ffffffff ffffffff ffffffff"),
+		alignment=4, # non-thumb
+		offset=0
+	)
+]
 
 #path = "../../r1_backup/r1 backup/lk_a.bin"
-path = "lk_b.bin"
-data = open(path, "rb").read()[0x200:]
 
-avb_hal_read_from_partition_ptr = find_pattern(data, read_partition_pattern)
-print(f"avb_hal_read_from_partition_ptr = {hex(avb_hal_read_from_partition_ptr)}")
+def test_patterns():
+	LK_DIR = "../dumped_bins/lk_versions"
 
-printf = find_pattern(data, printf_pattern)
-print(f"printf = {hex(printf)}")
+	for lk_name in sorted(os.listdir(LK_DIR), key=lambda x: x.split("-")[-1]):
+		print()
+		print(f"===== SCANNING {lk_name} =====")
+		with open(f"{LK_DIR}/{lk_name}", "rb") as lk_file:
+			lk_file.seek(0x200)
+			data = lk_file.read()
+		for pattern in PATTERNS:
+			addr = find_pattern(data, pattern, LK_BASE)
+			print(f"{pattern.name:<32} = {hex(addr)}")
 
-handle_vboot_state = find_pattern(data, handle_vboot_state_pattern)
-print(f"handle_vboot_state = {hex(handle_vboot_state)}")
+print("""
+struct pattern {
+	const char *name;
+	const unsigned char *pattern;
+	const unsigned char *caremap;
+	const size_t pattern_len;
+	const size_t alignment;
+	const size_t offset;
+};
 
-lcd_printf = find_pattern(data, lcd_printf_pattern)
-print(f"lcd_printf = {hex(lcd_printf)}")
+struct pattern PATTERNS[] = {""")
 
-memcpy = find_pattern(data, memcpy_pattern)
-print(f"memcpy = {hex(memcpy)}")
+for pattern in PATTERNS:
+	print("	{")
+	print(f'		.name = "{pattern.name}",')
+	print(f'		.pattern = {{{",".join(f"0x{x:02x}" for x in pattern.pattern)}}},')
+	print(f'		.caremap = {{{",".join(f"0x{x:02x}" for x in pattern.caremap)}}},')
+	print(f'		.pattern_len = {len(pattern.pattern)},')
+	print(f'		.alignment = {pattern.alignment},')
+	print(f'		.offset = {pattern.offset},')
+	print("	},")
+
+print("};")
